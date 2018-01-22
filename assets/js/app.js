f26a04eb3284636216fcaaed1a7192673ddb7c09
malditoprotozoo <virodriguez.fe@gmail.com>
2018-01-22 20:17:00 -0300

    Main functions are now in other file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,28 @@
-// $("#save-btn").click(function(text) {
-//   text = prompt("aksjdla");
-//   var blob = new Blob([text], {type: "text/plain;charset=utf-8"});
-//   saveAs(blob, "testfile1.txt");
-// });
+$(document).ready(function() {
+  $('#btn-submit').click(function() {
+    event.preventDefault();
+    appendResult(findNumber(), '#content-results');
+    $('#input-quote').val('');
+    $('.btn-coder').fadeIn();
+  });
+  $('#btn-to-img').click(function() {
+    event.preventDefault();
+    convertImg('#content-results', '#image-display');
+  });
+});
+
+/*
+let myString = `My string is ${true}`;
+undefined
+myString
+"My string is true"
+Service Worker termination by a timeout timer was canceled because DevTools is attached.
+myString = `My string is ${myString}`
+"My string is My string is true"
+myString = `My string is ${myString}`
+"My string is My string is My string is true"
+myString = `My string is ${myString}`
+"My string is My string is My string is My string is true"
+myString = `My string is ${myString}`
+"My string is My string is My string is My string is My string is true"
+ */
