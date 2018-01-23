9d7a3da600330a5f795b425e7019124500a30337
malditoprotozoo <virodriguez.fe@gmail.com>
2018-01-23 09:25:14 -0300

    Now the code decipher everything, but still needs some tweaks

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,31 @@ $(document).ready(function() {
     event.preventDefault();
     convertImg('#content-results', '#image-display');
   });
+  $('#btn-submit-chars').click(function() {
+    event.preventDefault();
+    decipherMurmurs();
+    //$('#results').append(`<p>`+decipherMurmurs()+`</p>`);
+    $('#input-chars').val('');
+    $('#input-code').val('');
+  })
+  $('#btn-step2').click(function() {
+    $('#instructions-step1').fadeOut();
+    $('#instructions-step2').fadeIn();
+    $('#form-step1').fadeOut();
+    $('#form-step2').fadeIn();
+    $('#btn-step1').fadeIn();
+    $('#content-results').fadeOut();
+    $(this).fadeOut();
+  });
+  $('#btn-step1').click(function() {
+    $('#instructions-step1').fadeIn();
+    $('#instructions-step2').fadeOut();
+    $('#form-step1').fadeIn();
+    $('#form-step2').fadeOut();
+    $('#btn-step2').fadeIn();
+    $('#content-results').fadeIn();
+    $(this).fadeOut();
+  });
 });
 
 /*
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const removeCharacters = text => {
-  let newText = text.replace(/ /g,'').replace(/\./g, '').toLowerCase();
+  let newText = text.replace(/ /g,'').replace(/\./g, '').replace(/,/g, '').toLowerCase();
   return newText;
 };
 
@@ -35,6 +35,36 @@ const convertImg = (element, where) => {
   });
 }
 
+const findLetter = (num, str) => {
+  return str.substring(num-1, num);
+}
+
+const deleteWhite = arr => {
+  if (arr[arr.length-1] == '') {
+    arr.pop();
+  }
+}
+
+const decipherMurmurs = (murmur, code) => {
+  murmur = $('#input-chars').val();
+  code = $('#input-code').val().split(',').map(element => Number(element));
+  let corrMurmur = murmur.replace(/ /g, '');
+  let result = [];
+  let pos = 0;
+  let num = code[pos];
+  for (let i = 0; i < corrMurmur.length; i++) {
+    if (findLetter(num, corrMurmur) !== '') {
+      result.push(findLetter(num, corrMurmur));
+    }
+    if (pos >= 2) {
+      pos = 0;
+    } else {
+      pos++;
+    }
+    num += code[pos];
+  }
+  appendResult(result.toString().replace(/,/g, ''), $('#results'));
+}
 
 
 // const convertImg = (element, where) => {
